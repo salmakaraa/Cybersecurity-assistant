c64ace9ab8835760a0a3a177bd8e94c74cf084c3
salma_karaa <salma.karaa@bertschinnovation.com>
2026-01-02 13:11:39 +0100

    corrected homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,7 +12,7 @@ class HomeController extends AbstractController
     {
        // $isAdmin = $this->isGranted('ROLE_ADMIN');
 
-        return $this->render('home/index.html.twig', [
+        return $this->render('home/home.html.twig', [
            // 'isAdmin' => $isAdmin,
         ]);
     }
