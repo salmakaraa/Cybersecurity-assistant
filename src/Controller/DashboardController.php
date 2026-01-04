da03363fa35016d7ba587821c1627f10eae12062
noolatifa <latifanoomen1602@gmail.com>
2026-01-05 00:33:45 +0100

    update

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\CodeScanChat;
 
 class DashboardController extends AbstractController
 {
@@ -16,7 +17,7 @@ class DashboardController extends AbstractController
     public function index(Request $request, EntityManagerInterface $em): Response
     {
         $user = $this->getUser();
-           $tab = $request->query->get('tab', 'articles'); // default to 'articles'
+        $tab = $request->query->get('tab', 'articles'); // default to 'articles'
         // Search functionality
         $search = $request->query->get('search', '');
         $articleRepo = $em->getRepository(Article::class);
@@ -29,19 +30,38 @@ class DashboardController extends AbstractController
             ->getResult();
 
         // Load favorites
-        $favorites = $user 
+        $favorites = $user
             ? $em->getRepository(Favorite::class)->findBy(['user' => $user])
             : [];
 
         // Create an array of favorite article IDs for easy lookup in Twig
         $favoriteIds = array_map(fn($f) => $f->getArticle()->getId(), $favorites);
 
+        // === 3. Get code scan chats ===
+        $chats = [];
+        if ($user) {
+            // Try to get chats from CodeScanChat entity
+            try {
+                $chatRepo = $em->getRepository(CodeScanChat::class);
+                $chats = $chatRepo->createQueryBuilder('c')
+                    ->where('c.user = :user')
+                    ->setParameter('user', $user)
+                    ->orderBy('c.updatedAt', 'DESC')
+                    ->setMaxResults(10) // Limit to 10 most recent
+                    ->getQuery()
+                    ->getResult();
+            } catch (\Exception $e) {
+                // If CodeScanChat doesn't exist yet, use empty array
+                $chats = [];
+            }
+        }
         return $this->render('dashboard/index.html.twig', [
             'active_tab' => $tab,
             'articles' => $articles,
             'favorites' => $favorites,
             'favoriteIds' => $favoriteIds,
             'search' => $search,
+            'chats' => $chats,
         ]);
     }
 
@@ -51,7 +71,7 @@ class DashboardController extends AbstractController
         $user = $this->getUser();
         if ($user) {
             $existing = $em->getRepository(Favorite::class)
-                           ->findOneBy(['user' => $user, 'article' => $article]);
+                ->findOneBy(['user' => $user, 'article' => $article]);
 
             if (!$existing) {
                 $favorite = new Favorite();
@@ -61,7 +81,7 @@ class DashboardController extends AbstractController
             }
         }
 
-        return $this->redirectToRoute('dashboard');
+        return $this->redirectToRoute('app_dashboard');
     }
 
     #[Route('/dashboard/favorite/remove/{id}', name: 'dashboard_remove_favorite')]
@@ -70,13 +90,13 @@ class DashboardController extends AbstractController
         $user = $this->getUser();
         if ($user) {
             $favorite = $em->getRepository(Favorite::class)
-                           ->findOneBy(['user' => $user, 'article' => $article]);
+                ->findOneBy(['user' => $user, 'article' => $article]);
             if ($favorite) {
                 $em->remove($favorite);
                 $em->flush();
             }
         }
 
-        return $this->redirectToRoute('dashboard');
+        return $this->redirectToRoute('app_dashboard');
     }
 }
